40cde80a1d6759ea4c8505d088dbb23d3443b381
ROGER RIBEIRO DE OLIVEIRA <25132662@CFP127ALUNO.LOCAL>
2025-09-09 16:27:33 -0300

    terminando

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const searchImg = document.getElementById('searchImg');
+
+async function buscarApiImagens (breed){
+    const url = `https://dog.ceo/api/breed/${breed}/images`;
+    const response = await fetch(url);
+    const photos = await response.json();
+    //console.log(photos.message);
+    return photos.message
+    
+}
+
+async function searchImage () {
+
+    const body = document.querySelector('main')
+    const containerPhotos = document.createElement('div')
+    containerPhotos.classList.add('containerPhotos')
+
+    const searched = document.getElementById('search').value
+
+    const dogImg = document.createElement('img')
+
+    const photo = await buscarApiImagens(searched);
+
+    photo.forEach((url) => {
+        const img = document.createElement('img')
+        img.src = url;
+        console.log(img)
+        containerPhotos.appendChild(img)
+        body.appendChild(containerPhotos)
+    });
+
+    
+}
+
+searchImg.addEventListener('click', searchImage)
